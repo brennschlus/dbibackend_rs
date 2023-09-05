5155090aa370e02bd1cbe141c939f61e81f8c9fe
brennschlus <brennschluss@protonmail.com>
2023-09-05 15:23:01 +0300

    args

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use crate::fs::File;
 use byteorder::{ByteOrder, LittleEndian};
 use rusb::{DeviceHandle, GlobalContext};
 use std::fs;
-use std::io::BufReader;
+
 use std::io::Read;
+use std::io::{BufReader, Seek, SeekFrom};
+use std::process::exit;
 use std::time;
 use std::time::Duration;
 use thiserror::Error;
@@ -12,18 +14,27 @@ const MAGIC: &[u8; 4] = b"DBI0";
 const VENDOR_ID: u16 = 0x057E;
 const ID_PRODUCT: u16 = 0x3000;
 const CMD_ID_EXIT: u32 = 0;
-//const CMD_ID_LIST_OLD: u16 = 1;
 const CMD_ID_FILE_RANGE: u32 = 2;
 const CMD_ID_LIST: u32 = 3;
+const BUFFER_SEGMENT_DATA_SIZE: usize = 0x100000;
 
 //const CMD_TYPE_REQUEST: u16 = 0;
 const CMD_TYPE_RESPONSE: u32 = 1;
 const CMD_TYPE_ACK: u32 = 2;
 
 fn main() {
-    let a = connect_to_switch();
+    let args: Vec<String> = std::env::args().collect();
 
-    match a {
+    if args.len() < 2 {
+        println!("Execpted folder path");
+        exit(0);
+    }
+
+    let path: &String = &args[1];
+
+    let switch_connection = connect_to_switch(path);
+
+    match switch_connection {
         Ok(_) => println!("fine"),
         Err(e) => println!("error: {:?}", e),
     }
@@ -39,6 +50,8 @@ enum DBIError {
     Parse(#[from] std::num::ParseIntError),
     #[error("utf8 errpr: {0}")]
     Utf8(#[from] std::str::Utf8Error),
+    #[error("wrong header. expected {:?}, found: {:?}", expected, found)]
+    WrongHeader { expected: [u8; 4], found: [u8; 4] },
 }
 
 struct OpenedDevice {
@@ -48,8 +61,8 @@ struct OpenedDevice {
 }
 
 impl OpenedDevice {
-    fn read(&self) -> Result<Vec<u8>, DBIError> {
-        let mut buf: Vec<u8> = vec![0; 1024];
+    fn read(&self, size: usize) -> Result<Vec<u8>, DBIError> {
+        let mut buf: Vec<u8> = vec![0; size];
         self.device
             .read_bulk(self.in_ep, &mut buf, Duration::from_secs(10))?;
         Ok(buf)
@@ -61,18 +74,13 @@ impl OpenedDevice {
     // }
 }
 
-// Define a function that opens a USB device by its vendor ID and product ID
 fn open_device(vid: u16, pid: u16) -> Result<OpenedDevice, rusb::Error> {
     let mut in_ep = 0;
     let mut out_ep = 0;
 
-    // Get a list of the current USB devices
     let devices = rusb::devices()?;
-    // Iterate over the devices
     for device in devices.iter() {
-        // Get the device descriptor
         let device_desc = device.device_descriptor()?;
-        // Check if the device matches the vendor ID and product ID
         if device_desc.vendor_id() == vid && device_desc.product_id() == pid {
             let config = device.active_config_descriptor()?;
 
@@ -89,7 +97,6 @@ fn open_device(vid: u16, pid: u16) -> Result<OpenedDevice, rusb::Error> {
                 }
             }
 
-            // Open the device and return the handle
             return Ok(OpenedDevice {
                 device: device.open()?,
                 in_ep,
@@ -97,28 +104,28 @@ fn open_device(vid: u16, pid: u16) -> Result<OpenedDevice, rusb::Error> {
             });
         }
     }
-    // If no matching device is found, return an error
     Err(rusb::Error::NotFound)
 }
 
-// Define a function that runs some functions based on received instructions
-fn run_functions(opened_device: &OpenedDevice, cmd: u32, data_size: u32) -> Result<(), DBIError> {
-    println!("run fuctions: {}", cmd);
-    // Match the command with different cases
+fn run_functions(
+    opened_device: &OpenedDevice,
+    cmd: u32,
+    data_size: u32,
+    path: &String,
+) -> Result<(), DBIError> {
     match cmd {
         CMD_ID_EXIT => {
-            process_exit_command();
+            process_exit_command(opened_device)?;
             Ok(())
         }
         CMD_ID_FILE_RANGE => {
-            proccess_file_range_command(&opened_device, data_size)?;
+            proccess_file_range_command(opened_device, data_size, path)?;
             Ok(())
         }
         CMD_ID_LIST => {
             process_list_command(
-                &opened_device.device,
-                opened_device.out_ep,
-                opened_device.in_ep,
+                opened_device,
+                path,
             )?;
             Ok(())
         }
@@ -131,31 +138,35 @@ fn run_functions(opened_device: &OpenedDevice, cmd: u32, data_size: u32) -> Resu
     }
 }
 
-fn connect_to_switch() -> Result<(), DBIError> {
+fn connect_to_switch(path: &String) -> Result<(), DBIError> {
     let opened_device = open_device(VENDOR_ID, ID_PRODUCT)?;
 
     loop {
-        let buf = opened_device.read()?;
+        let buf = opened_device.read(16)?;
 
-        println!("{:?}", buf.len());
         if buf.len() < 16 {
             return Err(DBIError::Rusb(rusb::Error::InvalidParam));
         }
 
         if !buf.starts_with(MAGIC) {
-            return Err(DBIError::Rusb(rusb::Error::InvalidParam));
+            let found: [u8; 4] = buf[..4].try_into().ok().ok_or(DBIError::WrongHeader {
+                expected: *MAGIC,
+                found: [0; 4],
+            })?;
+            return Err(DBIError::WrongHeader {
+                expected: *MAGIC,
+                found,
+            });
         }
 
         let cmd_id = LittleEndian::read_u32(&buf[8..12]);
         let data_size = LittleEndian::read_u32(&buf[12..16]);
 
-        match run_functions(&opened_device, cmd_id, data_size) {
-            // If successful, print a message and continue the loop
+        match run_functions(&opened_device, cmd_id, data_size, path) {
             Ok(_) => {
                 println!("Functions run successfully for command {}", cmd_id);
                 continue;
             }
-            // If failed, print an error and break the loop
             Err(e) => {
                 eprintln!("Error running functions for command {}: {}", cmd_id, e);
                 break Err(e);
@@ -164,11 +175,24 @@ fn connect_to_switch() -> Result<(), DBIError> {
     }
 }
 
-fn process_exit_command() {
-    println!("exit")
+fn process_exit_command(device: &OpenedDevice) -> Result<(), DBIError> {
+    let mut buffer: Vec<u8> = Vec::new();
+    buffer.extend_from_slice(MAGIC);
+    buffer.extend_from_slice(&CMD_TYPE_RESPONSE.to_le_bytes());
+    buffer.extend_from_slice(&CMD_ID_EXIT.to_le_bytes());
+    buffer.extend_from_slice(&CMD_ID_EXIT.to_le_bytes());
+
+    device
+        .device
+        .write_bulk(device.out_ep, &buffer, time::Duration::from_secs(10))?;
+    exit(1);
 }
 
-fn proccess_file_range_command(device: &OpenedDevice, data_size: u32) -> Result<(), DBIError> {
+fn proccess_file_range_command(
+    device: &OpenedDevice,
+    data_size: u32,
+    path: &String,
+) -> Result<(), DBIError> {
     let mut buffer: Vec<u8> = Vec::new();
     buffer.extend_from_slice(MAGIC);
     buffer.extend_from_slice(&CMD_TYPE_ACK.to_le_bytes());
@@ -181,10 +205,10 @@ fn proccess_file_range_command(device: &OpenedDevice, data_size: u32) -> Result<
 
     buffer.clear();
     // Read from the bulk endpoint with a 10 second timeout
-    let buf = device.read().unwrap();
+    let buf = device.read(data_size as usize)?;
 
     let range_size = LittleEndian::read_u32(&buf[..4]);
-    let range_offset = LittleEndian::read_u32(&buf[4..12]);
+    let range_offset = LittleEndian::read_u64(&buf[4..12]);
     let nsp_name_len = LittleEndian::read_u32(&buf[12..16]);
     let nsp_name = std::str::from_utf8(&buf[16..])?;
     println!(
@@ -195,14 +219,14 @@ fn proccess_file_range_command(device: &OpenedDevice, data_size: u32) -> Result<
     buffer.extend_from_slice(MAGIC);
     buffer.extend_from_slice(&CMD_TYPE_RESPONSE.to_le_bytes());
     buffer.extend_from_slice(&CMD_ID_FILE_RANGE.to_le_bytes());
-    buffer.extend_from_slice(&data_size.to_le_bytes());
+    buffer.extend_from_slice(&range_size.to_le_bytes());
 
     device
         .device
         .write_bulk(device.out_ep, &buffer, time::Duration::from_secs(10))?;
 
     buffer.clear();
-    let buf = device.read().unwrap();
+    let buf = device.read(16)?;
     let ack = LittleEndian::read_u32(&buf[..4]);
     let cmd_type = LittleEndian::read_u32(&buf[4..8]);
     let cmd_id = LittleEndian::read_u32(&buf[8..12]);
@@ -213,42 +237,44 @@ fn proccess_file_range_command(device: &OpenedDevice, data_size: u32) -> Result<
         cmd_type, cmd_id, data_size
     );
     println!("{ack}");
+    //let filen_name_with_space = nsp_name.replace("\0", " ");
 
-    let path = format!(
-        "/home/pavel/Games/Nintendo/Super Mario 3D All-Stars [NSP]/{}",
-        nsp_name
-    );
+
+    let file_path: String = path.to_owned() + "/" + nsp_name;
 
     // Open the file and create a buffered reader
-    let file = File::open(path)?;
+    let file = File::open(file_path)?;
     let mut reader: BufReader<File> = BufReader::new(file);
 
-    // Create a buffer with the chunk size capacity
-    let mut buffer = vec![0; data_size as usize];
+    reader.seek(SeekFrom::Start(range_offset))?;
+    let mut curr_off: u32 = 0x0;
+    let end_off = range_size;
+    let mut read_size: u32 = BUFFER_SEGMENT_DATA_SIZE as u32;
 
-    // Loop until the end of the file is reached
-    loop {
-        // Read a chunk of data from the file into the buffer
-        let n = reader.read(&mut buffer)?;
-
-        // Break the loop if no more data is read
-        if n == 0 {
-            break Ok(());
+    while curr_off < end_off {
+        if curr_off + read_size >= end_off {
+            read_size = end_off - curr_off;
         }
 
-        // Write the buffer to the device using write_bulk method with a 10 second timeout
+        let mut buffer: Vec<u8> = vec![0; read_size as usize];
+
+        reader.read(&mut buffer)?;
+
         device
             .device
-            .write_bulk(device.out_ep, &buffer[..n], time::Duration::from_secs(10))?;
+            .write_bulk(device.out_ep, &buffer, time::Duration::from_secs(0))?;
+
+        curr_off += read_size;
     }
+
+    Ok(())
 }
 
 fn process_list_command(
-    device: &DeviceHandle<GlobalContext>,
-    out_ep: u8,
-    in_ep: u8,
+    device: &OpenedDevice,
+    path: &String,
 ) -> Result<(), DBIError> {
-    let entries = fs::read_dir("/home/pavel/Games/Nintendo/Super Mario 3D All-Stars [NSP]")?;
+    let entries = fs::read_dir(path)?;
     let file_names: Vec<String> = entries
         .filter_map(|entry| {
             let path = entry.ok()?.path();
@@ -270,14 +296,13 @@ fn process_list_command(
 
     buffer.extend_from_slice(&(data_size as u32).to_le_bytes());
 
-    device.write_bulk(out_ep, buffer.as_slice(), time::Duration::from_secs(10))?;
+
+    device.device.write_bulk(device.out_ep, buffer.as_slice(), time::Duration::from_secs(10))?;
 
     buffer.clear();
 
-    let mut buf = vec![0; 1024];
 
-    // Read from the bulk endpoint with a 10 second timeout
-    let _n = device.read_bulk(in_ep, &mut buf, time::Duration::from_secs(10))?;
+    let buf = device.read(16)?;
 
     println!(
         "Cmd Type: {:?}, Command id: {:?}, Data size: {:?}",
@@ -292,6 +317,6 @@ fn process_list_command(
         buffer.extend_from_slice(file_name.as_bytes());
         buffer.push(b'\n');
     }
-    device.write_bulk(out_ep, buffer.as_slice(), time::Duration::from_secs(50))?;
+    device.device.write_bulk(device.out_ep, buffer.as_slice(), time::Duration::from_secs(50))?;
     Ok(())
 }
